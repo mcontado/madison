5fdf532823e2a4f017e37571c9a5c4d51e8c3d7f
feech <andreev.kirill.o@gmail.com>
2016-03-26 17:08:59 +0100

    fixed

--- js/form.js
+++ js/form.js
@@ -1,27 +1,34 @@
 $(document).ready(function(){
 
 	$(".slider > img").fadeIn(0);
-	$("#01, #11, #21, #31").fadeOut(0);
+	// $("#01, #11, #21, #31").fadeOut(0);
+
+
 	$("#01").fadeIn(0);
-	$("#02").fadeIn(0);
-	$("#03").fadeIn(0);
-	$("#04").fadeIn(0);
+	$("#11").fadeIn(0);
+	$("#21").fadeIn(0);
+	$("#31").fadeIn(0);
 
-	$("#11").fadeOut(0);
+	$("#02").fadeOut(0);
 	$("#12").fadeOut(0);
-	$("#13").fadeOut(0);
-	$("#14").fadeOut(0);
-
-
-	$("#21").fadeOut(0);
 	$("#22").fadeOut(0);
-	$("#23").fadeOut(0);
-	$("#24").fadeOut(0);
-
-	$("#31").fadeOut(0);
 	$("#32").fadeOut(0);
+
+	$("#03").fadeOut(0);
+	$("#13").fadeOut(0);
+	$("#23").fadeOut(0);
 	$("#33").fadeOut(0);
+
+
+	$("#04").fadeOut(0);
+	$("#14").fadeOut(0);
+	$("#24").fadeOut(0);
 	$("#34").fadeOut(0);
+
+	// $("#01").fadeOut(0);
+	// $("#12").fadeOut(0);
+	// $("#23").fadeOut(0);
+	// $("#34").fadeOut(0);
 	
 	// myLoop(4,1,0);
 	// myLoop(4,1,2);
@@ -36,17 +43,21 @@ $(document).ready(function(){
 	}, 200)
 });
 
-function slider(panel, position){
-	console.log(panel, position)
-	p = panel%4
+// picture_idx 0-3
+function slider(picture_idx, position){
+	console.log(picture_idx, position)
+	p = picture_idx%4
 	pn = (p+1)%4
+
+	idx = p+1
+	idxn = pn +1
 
 	if(position)
 	{
-		$("#"+p+"1").fadeOut(0)
-		$("#"+p+"3").fadeOut(0)
-		$("#"+pn+"1").fadeIn(300)
-		$("#"+pn+"3").fadeIn(300)
+		$("#0"+idx).fadeOut(0)
+		$("#2"+idx).fadeOut(0)
+		$("#0"+idxn).fadeIn(300)
+		$("#2"+idxn).fadeIn(300)
 		setTimeout(function()
 		{
 			slider(p, false)
@@ -54,10 +65,10 @@ function slider(panel, position){
 	}
 	else
 	{
-		$("#"+p+"2").fadeOut(0)
-		$("#"+p+"4").fadeOut(0)
-		$("#"+pn+"2").fadeIn(300)
-		$("#"+pn+"4").fadeIn(300)
+		$("#1"+idx).fadeOut(0)
+		$("#3"+idx).fadeOut(0)
+		$("#1"+idxn).fadeIn(300)
+		$("#3"+idxn).fadeIn(300)
 
 		setTimeout(function()
 		{
