3cbaa16b9f248c9e38d6865a0bebb00290ad5954
feech <andreev.kirill.o@gmail.com>
2016-03-26 16:27:53 +0100

    another algorithm to switching pictures

--- js/form.js
+++ js/form.js
@@ -1,29 +1,70 @@
 $(document).ready(function(){
 
-	$(".slider > img").fadeOut(0);
-	$("#01, #11, #21, #31").fadeIn(0);
+	$(".slider > img").fadeIn(0);
+	$("#01, #11, #21, #31").fadeOut(0);
+	$("#01").fadeIn(0);
+	$("#02").fadeIn(0);
+	$("#03").fadeIn(0);
+	$("#04").fadeIn(0);
+
+	$("#11").fadeOut(0);
+	$("#12").fadeOut(0);
+	$("#13").fadeOut(0);
+	$("#14").fadeOut(0);
+
+
+	$("#21").fadeOut(0);
+	$("#22").fadeOut(0);
+	$("#23").fadeOut(0);
+	$("#24").fadeOut(0);
+
+	$("#31").fadeOut(0);
+	$("#32").fadeOut(0);
+	$("#33").fadeOut(0);
+	$("#34").fadeOut(0);
 	
-	myLoop(4,1,0);
-	myLoop(4,1,2);
-	setTimeout(myLoop(4,1,1), 2000);
-	setTimeout(myLoop(4,1,3), 2000);
+	// myLoop(4,1,0);
+	// myLoop(4,1,2);
+	// setTimeout(myLoop(4,1,1), 2000);
+	// setTimeout(myLoop(4,1,3), 2000);
+	// console.log([1,2,3,4,6], true, !true)
+	// slider(1,2)
+
+	setTimeout(function()
+	{
+		slider(0, true)
+	}, 200)
 });
 
 function slider(panel, position){
-	if (position != 1) {
-		$("#"+panel+(position-1)).fadeOut(0);
-		$("#"+panel+position).fadeIn(300);
+	console.log(panel, position)
+	p = panel%4
+	pn = (p+1)%4
+
+	if(position)
+	{
+		$("#"+p+"1").fadeOut(0)
+		$("#"+p+"3").fadeOut(0)
+		$("#"+pn+"1").fadeIn(300)
+		$("#"+pn+"3").fadeIn(300)
+		setTimeout(function()
+		{
+			slider(p, false)
+		}, 1000)
 	}
-	else {
-		$("#"+panel+(($("img").size() -2)/4)).fadeOut(0);
-		$("#"+panel+position).fadeIn(300);
+	else
+	{
+		$("#"+p+"2").fadeOut(0)
+		$("#"+p+"4").fadeOut(0)
+		$("#"+pn+"2").fadeIn(300)
+		$("#"+pn+"4").fadeIn(300)
+
+		setTimeout(function()
+		{
+			slider(pn, true)
+		}, 8000)
+
 	}
+
 }
 
-function myLoop (iterations, current, panel) {          
-   setTimeout(function () {   
-      slider(panel, current);
-      if (current < iterations) myLoop(iterations, ++current, panel);
-      else myLoop(iterations, 1, panel);
-   }, 8000)
-}        
