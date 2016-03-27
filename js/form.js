a79fb8847b8f7a01208eaa5f6bb64c5354aa5a48
chad187 <chad187@gmail.com>
2016-03-26 20:07:40 -0700

    cycling is working

--- js/form.js
+++ js/form.js
@@ -1,29 +1,21 @@
 $(document).ready(function(){
 
-	$(".slider > img").fadeIn(0);
-	// $("#01, #11, #21, #31").fadeOut(0);
+	$(".slider > img").fadeOut(0);
 
 
-	$("#01").fadeIn(0);
-	$("#11").fadeIn(0);
-	$("#21").fadeIn(0);
-	$("#31").fadeIn(0);
+	// $("#01").fadeIn(0);
+	// $("#11").fadeIn(0);
+	// $("#21").fadeIn(0);
+	// $("#31").fadeIn(0);
 
-	$("#02").fadeOut(0);
-	$("#12").fadeOut(0);
-	$("#22").fadeOut(0);
-	$("#32").fadeOut(0);
+	$('.slider2, .slider4').cycle({
+          timeout: 8000,
+        });
 
-	$("#03").fadeOut(0);
-	$("#13").fadeOut(0);
-	$("#23").fadeOut(0);
-	$("#33").fadeOut(0);
-
-
-	$("#04").fadeOut(0);
-	$("#14").fadeOut(0);
-	$("#24").fadeOut(0);
-	$("#34").fadeOut(0);
+	$('.slider1, .slider3').cycle({
+            delay: 2000,
+            timeout: 8000,
+          });
 
 	// $("#01").fadeOut(0);
 	// $("#12").fadeOut(0);
@@ -37,45 +29,45 @@ $(document).ready(function(){
 	// console.log([1,2,3,4,6], true, !true)
 	// slider(1,2)
 
-	setTimeout(function()
-	{
-		slider(0, true)
-	}, 200)
+	// setTimeout(function()
+	// {
+	// 	slider(0, true)
+	// }, 200)
 });
 
 // picture_idx 0-3
-function slider(picture_idx, position){
-	console.log(picture_idx, position)
-	p = picture_idx%4
-	pn = (p+1)%4
+// function slider(picture_idx, position){
+// 	console.log(picture_idx, position)
+// 	p = picture_idx%4
+// 	pn = (p+1)%4
 
-	idx = p+1
-	idxn = pn +1
+// 	idx = p+1
+// 	idxn = pn +1
 
-	if(position)
-	{
-		$("#0"+idx).fadeOut(0)
-		$("#2"+idx).fadeOut(0)
-		$("#0"+idxn).fadeIn(300)
-		$("#2"+idxn).fadeIn(300)
-		setTimeout(function()
-		{
-			slider(p, false)
-		}, 1000)
-	}
-	else
-	{
-		$("#1"+idx).fadeOut(0)
-		$("#3"+idx).fadeOut(0)
-		$("#1"+idxn).fadeIn(300)
-		$("#3"+idxn).fadeIn(300)
+// 	if(position)
+// 	{
+// 		$("#0"+idx).fadeOut(0)
+// 		$("#2"+idx).fadeOut(0)
+// 		$("#0"+idxn).fadeIn(3000)
+// 		$("#2"+idxn).fadeIn(3000)
+// 		setTimeout(function()
+// 		{
+// 			slider(p, false)
+// 		}, 1000)
+// 	}
+// 	else
+// 	{
+// 		$("#1"+idx).fadeOut(0)
+// 		$("#3"+idx).fadeOut(0)
+// 		$("#1"+idxn).fadeIn(3000)
+// 		$("#3"+idxn).fadeIn(3000)
 
-		setTimeout(function()
-		{
-			slider(pn, true)
-		}, 8000)
+// 		setTimeout(function()
+// 		{
+// 			slider(pn, true)
+// 		}, 3000)
 
-	}
+// 	}
 
-}
+// }
 
