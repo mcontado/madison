c4c4cac40866264c4f649bb2cffca00b8f4347a9
chad187 <chad187@gmail.com>
2016-03-28 09:26:27 -0700

    removing things to determine why some things are hidden

--- js/form.js
+++ js/form.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
 
-	$(".slider > img").fadeOut(0);
+	$(".slider1 > img").fadeOut(0);
+	$(".slider2 > img").fadeOut(0);
+	$(".slider3 > img").fadeOut(0);
+	$(".slider4 > img").fadeOut(0);
 
 
 	// $("#01").fadeIn(0);
@@ -16,18 +19,6 @@ $(document).ready(function(){
             delay: 2000,
             timeout: 8000,
           });
-
-	// $("#01").fadeOut(0);
-	// $("#12").fadeOut(0);
-	// $("#23").fadeOut(0);
-	// $("#34").fadeOut(0);
-	
-	// myLoop(4,1,0);
-	// myLoop(4,1,2);
-	// setTimeout(myLoop(4,1,1), 2000);
-	// setTimeout(myLoop(4,1,3), 2000);
-	// console.log([1,2,3,4,6], true, !true)
-	// slider(1,2)
 
 	// setTimeout(function()
 	// {
